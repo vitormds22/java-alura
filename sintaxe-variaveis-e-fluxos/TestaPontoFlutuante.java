625b0aec7a725a23238410f2f46d2e3c57adf0d8
Vitor Machado <machado@pop-os.localdomain>
2022-02-04 23:05:18 -0300

    Aprendendo um pouco sobre o tipo double

---
+++ sintaxe-variaveis-e-fluxos/TestaPontoFlutuante.java
@@ -0,0 +1,24 @@
+public class TestaPontoFlutuante {
+      public static void main (String [] args){
+            double salario;
+		salario = 1250.70;
+			
+		System.out.println("Meu salário é: " + salario);
+		
+		// Podemos usar inteiros para tipo de ponto flutuante
+		// double idade = 37;
+		
+		// Podemos fazer contas dentro da variavel
+		double divisao = 3.14 / 2;
+		System.out.println("Divisão de um ponto flutuante por inteiro: " + divisao);
+		
+		// Podemos fazer até divisão de inteiros que o resutado seria com ponto flutuante
+		// Mas o java força o retorno inteiro :(
+		int outraDivisao = 5 / 2;
+		System.out.println("Divisão de inteiros: " + outraDivisao);
+		
+		// Mas para termos o resultado correto fazemos assim
+		double novaTentativa = 5.0 / 2;
+		System.out.println("Divisão de um double por int " + novaTentativa);
+      }
+}
