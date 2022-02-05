924a185c5f32abe3a7e9c7333122a2cf0e00bbd9
Vitor Machado <machado@pop-os.localdomain>
2022-02-04 23:07:45 -0300

    Aprendendo sobre o tipo int

---
+++ sintaxe-variaveis-e-fluxos/TestaVariaveis.java
@@ -0,0 +1,17 @@
+public class TestaVariaveis {
+      public static void main(String [] args){
+            System.out.println("===========");
+		int idade = 24;
+		System.out.println(idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		 
+		idade = 7 * 5 + 2;
+		
+		System.out.println(idade);
+
+		System.out.println("a idade é: " + idade);
+      }
+}
