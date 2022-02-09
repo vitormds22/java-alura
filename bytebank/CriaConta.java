ab28af303f33e5edef9ce608a64829f63bee83e9
Vitor Machado <machado@pop-os.localdomain>
2022-02-08 23:54:26 -0300

    Começando orientação a objeto com java

---
+++ bytebank/CriaConta.java
@@ -0,0 +1,9 @@
+public class CriaConta {
+      public static void main(String[] args) {
+      //      CRIANDO UM OBJETO DO TIPO CONTA
+            Conta primeiraConta = new Conta();
+            primeiraConta.saldo = 200;
+
+            System.out.println(primeiraConta.saldo);
+      }
+}
