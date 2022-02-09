a99177692c8910236f57b3611e4510aa3cbd3721
Vitor Machado <machado@pop-os.localdomain>
2022-02-09 00:05:54 -0300

    Trabalhando com a classe Conta e inserindo valores aos seus atributos

--- bytebank/CriaConta.java
+++ bytebank/CriaConta.java
@@ -3,7 +3,14 @@ public class CriaConta {
       //      CRIANDO UM OBJETO DO TIPO CONTA
             Conta primeiraConta = new Conta();
             primeiraConta.saldo = 200;
-
             System.out.println(primeiraConta.saldo);
+            primeiraConta.saldo += 100;
+            
+            Conta segundaConta = new Conta();
+
+            segundaConta.saldo = 500;
+            System.out.println("Na primeira conta tem: " + primeiraConta.saldo);
+            System.out.println("Na segunda conta tem: " + segundaConta.saldo);
+            
       }
 }
---
+++ bytebank/TesteReferencia.java
@@ -0,0 +1,5 @@
+public class TesteReferencia {
+      public static void main(String[] args) {
+            
+      }
+}
